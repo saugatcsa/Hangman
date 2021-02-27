8f50a07105329d29b684b7f92cd81d3215b5bbfa
Saugat Bhattarai <saugatcsa@gmail.com>
2021-02-27 19:54:13 +0000

    fixed, same character on the word

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
 #hangman
 import collections 
 def notwon():
-  if collections.Counter(list) == collections.Counter(newlist):
+  if collections.Counter(alist) == collections.Counter(newlist):
     return False
   else:
     return True
 
 # # checking for humam error ---  still working on it 
 def checkinput(x):
-  # checking for humam error
-  # if number 
+  # if its number 
   if (len(x) == 1):
     return x
 
 # store the answer in a list 
-list = []
-word = "TESLA"
+alist = []
+word = "TEE"
 for x in word:
-  list.append(x)
-#print(list) #testing 
+  alist.append(x)
+print(alist) #testing 
 
 # creating a empty list with _ with same lenth   
 newlist = []
 for i in range(0,(len(word))):
   newlist.append("_")
-#print(newlist) #testing 
+print(newlist) #testing 
 
 counter = 0
 while (counter < 5 and notwon()):
   print()
   guess = input("Enter a letter: ")
-  
+# use checkinput function here 
   guess = guess.upper()
   success = 0
-  for x in word:
-     
-    if x == guess :
-      newlist[list.index(x)] = guess
+  for x in range(0, len(word)):
+    if word[x] == guess:
+      newlist[x] = guess
       #print(newlist) #testing 
       success += 1
 
-      for i in newlist:
-        print(i, end =" ",)
+
     
   if success >= 1:
-    print("Yes, way to go")
+    for i in newlist:
+      print(i, end =" ",)
+    print("  :)  way to go")
   else:
     counter += 1
-    print(f"Oh NO, you have {5-counter} more try left")
+    print(f"Oh NO, you have {5-counter} more tries left")
       
 if notwon() == False:
   print()
